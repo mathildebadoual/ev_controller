5126fecfa511c43f51df163b38cee1bb3ece5ff5
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-01-27 15:04:48 -0800

    add states.py

---
+++ ev_controller/states.py
@@ -0,0 +1,31 @@
+""" State Machine """
+
+
+class StateMachine():
+    def __init__(self):
+        pass
+
+
+class State():
+    def __init__(self):
+        pass
+
+
+class CarIsGone():
+    def __init__(self):
+        pass
+
+
+class CarIsPlugged(State):
+    def __init__(self):
+        pass
+
+
+class CarCharging(State):
+    def __init__(self):
+        pass
+
+
+class CarStopCharging(State):
+    def __init__(self):
+        pass
