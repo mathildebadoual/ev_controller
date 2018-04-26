0f21ab49e72c67d4d3ace1a656c32445bb84c12d
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-04-26 09:35:18 -0700

    add figures

--- data/markov_chain_cars.py
+++ data/markov_chain_cars.py
@@ -7,9 +7,10 @@ import sqlite3
 import itertools
 import pandas as pd
 from scipy.interpolate import interp1d
+import mpl_toolkits.mplot3d.axes3d as p3
 
 horizon = 48
-num_interp = 20
+num_interp = 2
 
 data_ev = pd.read_csv('/Users/mathildebadoual/code/ev_controller/data/EV_data.csv')
 ID_26 = data_ev['ID:26']     #Car_ID:26
@@ -49,24 +50,48 @@ for k in range(23):  # 23 cuz probability doesn't count for last timestep
 
 P = np.array(p_list)
 
-imagelist = [P[k, :, :] for k in range(P.shape[2])]
+imagelist = [P[k, :, :] for k in range(23)]
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-fig = plt.figure() # make figure
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+X, Y = np.meshgrid(range(num_interp), range(num_interp))
+X = X.flatten('F')
+Y = Y.flatten('F')
+Z = np.zeros_like(X)
+
+dx = 0.5 * np.ones_like(Z)
+dy = dx.copy()
 
 # make axesimage object
 # the vmin and vmax here are very important to get the color map correct
-im = plt.imshow(imagelist[0], cmap=plt.get_cmap('Reds'))
+#im = plt.imshow(imagelist[0], cmap=plt.get_cmap('Reds'))
+
+im = ax.bar3d(X, Y, Z, dx, dy, imagelist[0].flatten())
+
+row_names = ['', '0', '', '', '', '', '1', '']
+column_names = ['', '0', '', '', '', '', '1', '']
 
 # function to update figure
 def updatefig(j):
+    ax.cla()
+    ax.set_zlim(0, 1)
     # set the data in the axesimage object
-    im.set_array(imagelist[j])
+    im = ax.bar3d(X, Y, Z, dx, dy, imagelist[j].flatten())
+    ax.w_yaxis.set_ticklabels(row_names)
+    ax.w_xaxis.set_ticklabels(column_names)
+    ax.set_xlabel('Past state')
+    ax.set_ylabel('Next state')
+    ax.set_zlabel('Probability')
+    ax.set_title('Probability of moving from one state to another at hour : ' + str(j) + ' of the day')
     # return the artists set
     return [im]
 # kick off the animation
-ani = animation.FuncAnimation(fig, updatefig, frames=range(len(imagelist)),
+
+ani = animation.FuncAnimation(fig, updatefig, frames=range(23),
                               interval=1000)
 ani.save('/Users/mathildebadoual/code/ev_controller/report/cars.mp4', writer='ffmpeg')
